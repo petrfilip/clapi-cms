088f87baa9ef568f268a2250a22045ea38b51538
Petr Filip <petr.filip@tix.cz>
2020-06-21 14:26:57 +0200

    Fixed ordering of text-area

--- admin/src/components/content-editor/components/text-area.js
+++ admin/src/components/content-editor/components/text-area.js
@@ -1,71 +1,31 @@
 import { React } from 'preact'
-import { init } from 'pell'
-import { useEffect } from 'preact/hooks'
-import css from './pell.css'
-import Input from '../../elementary/input'
+import RichTextArea from 'preact-richtextarea'
+import { useRef } from 'preact/hooks'
 
-function generateRandomString() {
-  return Math.random().toString(36).substring(7)
-}
 function getOnInput(props) {
   return (event) => {
     ;(props.onInput && props.onInput(event)) ||
-      (props.onInputChangeCallback && props.onInputChangeCallback(props.id, event.target.value))
+      (props.onInputChangeCallback && props.onInputChangeCallback(props.id, value))
   }
 }
 
 const TextArea = (props) => {
-  // const editorId = generateRandomString() + '-editor'
-  // useEffect(() => {
-  //   const pell = init({
-  //     element: document.getElementById(editorId),
-  //     onChange: (newHtml) => props.onInputChangeCallback(props.id, newHtml),
-  //     defaultParagraphSeparator: 'p',
-  //     actions: [
-  //       'bold',
-  //       'underline',
-  //       'italic',
-  //       'olist',
-  //       'ulist',
-  //       {
-  //         name: 'link',
-  //         result: (val) => {
-  //           alert('TODO' + val)
-  //         },
-  //       },
-  //     ],
-  //     classes: {
-  //       actionbar: css.pellActionbar,
-  //       button: css.pellButton,
-  //       content: css.pellContent,
-  //       selected: css.pellButtonSelected,
-  //     },
-  //   })
-  //   pell.content.innerHTML = props.initialValue || ''
-  //
-  //   pell.addEventListener('paste', function (e) {
-  //     e.preventDefault()
-  //
-  //     if (e.clipboardData) {
-  //       const content = e.clipboardData.getData('text/plain')
-  //       document.execCommand('insertText', false, content)
-  //     }
-  //   })
-  // }, [props.index])
+  const inputRef = useRef(null)
 
   return (
-    <textarea
-      value={props.initialValue || ''}
-      key={[props.index, props.id, props.type].join('_')}
+    <RichTextArea
+      ref={inputRef}
+      onpaste={(e) => {
+        e.preventDefault()
+        if (e.clipboardData) {
+          const content = e.clipboardData.getData('text/plain')
+          inputRef.current.execCommand('insertText', false, content)
+        }
+      }}
+      value={props.initialValue}
       onInput={getOnInput(props)}
     />
   )
-
-  // return (
-  //   <div className={css.pell}>
-  //     <div id={editorId} className="pell" />
-  //   </div>
-  // )
 }
 
 export default TextArea
